f2ad9d210772107d8a16c8245488ebdb30e8d3b5
Chayanit <chayanit.chaisri@gmail.com>
2021-09-16 12:49:20 -0400

    Finished the user authentication

--- server/server.js
+++ server/server.js
@@ -1,41 +1,39 @@
 const express = require("express");
-const cors = require('cors');
+const cors = require("cors");
 const jwt = require("jsonwebtoken");
 const { USERFRONT_PUBLIC_KEY } = require("./environment");
 
 const app = express();
 // Then use it before your routes are set up:
-app.use(cors({ credentials: true }));
+app.use(cors());
 
 const authenticateToken = async (req, res, next) => {
   // Read the JWT access token from the request header
   const authHeader = req.headers.authorization;
-  
   // Authorization: Bearer dsfdsfdsf - choose dsfdsfdsf like [1]
-  const token = authHeader && authHeader.split(" ")[1]  ;
-
+  const token = authHeader && authHeader.split(' ')[1];
   if (token == null) {
     return res.status(401).json({
-      message: "Bad token",
+      message: 'Bad token',
     });
+  }
 
-    // Verify the token using the Userfront public key
-    try {
-      const auth = await jwt.verify(token, USERFRONT_PUBLIC_KEY);
-      req.auth = auth;
-      next();
-    } catch (error) {
-      return res.status(401).json({
-        message: "Bad token",
-      });
-    }
+  // Verify the token using the Userfront public key
+  try {
+        const auth = await jwt.verify(token, USERFRONT_PUBLIC_KEY);
+    req.auth = auth;
+    next();
+  } catch (error) {
+    return res.status(401).json({
+        message: 'Bad token',
+    });
   }
 };
 
 app.get('/data', authenticateToken, (req, res) => {
-    return res.json({
-      someSecretData: 'Shhhh!',
-    });
+  return res.json({
+    someSecretData: 'Shhhh!',
   });
+});
 
-app.listen(3010, () => console.log("App listening on port 3010"));
+app.listen(3010, () => console.log('App listening on port 3010'));
